e98c7759c89382434f0e403d2f86ba687315aefa
virusimmortal00 <rsayers88@gmail.com>
2017-02-10 17:53:52 -0500

    Added music and content pages WITH working animation
    holy fuck what a day

--- js/tools.js
+++ js/tools.js
@@ -1,17 +1,50 @@
-//This function changes the display property of code within the "content" div
-function change_page(pageName)
+$( document ).ready(function() {
+
+$(document).on('click', '.iconBox', function() {
+var linkSelection= $(this).attr('class').split(' ')[1];
+if(linkSelection=="icon-soundcloud")
 {
-	var nameList = ["music","video","pics","merch","home"];
-	var pos = nameList.indexOf(pageName);
-	for(x=0;x<4;x++)
-	{
-		if(x!=pos)
-		{	
-			document.getElementById(nameList[x]).style.display = "none";	
-		}
-		else
-		{
-			document.getElementById(pageName).style.display = "inherit";
-		}
-	}					
+	window.open("http://rpa.life/2lhz5yK");
 }
+if(linkSelection=="icon-facebook-squared")
+{
+	window.open("http://rpa.life/2lhC1vm");
+}
+if(linkSelection=="icon-instagram")
+{
+	window.open("http://rpa.life/2lhL2Vj");
+}
+if(linkSelection=="icon-youtube-squared")
+{
+	window.open("http://rpa.life/2kfBR7n");
+}
+});
+
+	$(document).on('click', '.music', function() {
+		var pos = $(".musicContent").css("transform");
+		if(pos == "matrix(1, 0, 0, 1, 0, 0)")
+		{
+			$(".musicContent").css("transform","translateY(2000px)");
+		}
+		else {
+			$(".videoContent").css("transform","translateY(2000px)");
+			$(".musicContent").css("transform","translateY(0px)");
+		}
+	});
+
+	$(document).on('click', '.videos', function() {
+	var pos = $(".videoContent").css("transform");
+	console.log("video");
+	console.log(pos);
+	if(pos == "matrix(1, 0, 0, 1, 0, 0)")
+	{
+		$(".videoContent").css("transform","translateY(2000px)");
+
+	}
+	else {
+		$(".musicContent").css("transform","translateY(2000px)");
+		$(".videoContent").css("transform","translateY(0px)");
+	}
+});
+
+});
