1778ffc1303a6784475a2a8e37526bf957a808ae
virusimmortal00 <rsayers88@gmail.com>
2017-05-02 10:33:23 -0400

    Pageswitch optimized 5/2/17
    Finally finished making pageswitch dry and elegant

--- js/pageswitch.js
+++ js/pageswitch.js
@@ -1,73 +1,44 @@
-var navSelected = "#musicli";
 
-$('.music').click( function() {
-  var pos = $(".musicContent").css("transform");
+function moveTabs(page){
+  var navOptions = $('.leftMenu-nav li');
+  var navArray = $.makeArray(navOptions);
+
+  var rightContentDivs = $('div[class*="Content"]');
+  var contentNodes = [].slice.call(rightContentDivs, 1);
+  var skip = contentNodes[0].className;
+  var newRay = [];
+  for(i = 0; i<contentNodes.length; i++){
+    if(contentNodes[i].className != `${page}Content`)
+    {
+      newRay.push(contentNodes[i].className);
+    }
+  }
+  var pos = $(`.${page}Content`).css("transform");
+  //If the position of the current tab is not in the middle, then bring it to center
   if(pos != "matrix(1, 0, 0, 1, 0, 0)")
   {
-    $(".videoContent").css("transform","translateY(2000px)");
-    $(".picContent").css("transform","translateY(2000px)");
-    $(".bioContent").css("transform","translateY(2000px)");
-    $(".musicContent").css("transform","translateY(0px)");
-    $(navSelected).toggleClass("selected");
-    $('#musicli').toggleClass("selected");
-    navSelected = "#musicli";
+    for(i=0; i <newRay.length; i++){
+      $(`.${newRay[i]}`).css("transform","translateY(2000px)");
+    }
+    $(`.${page}Content`).css("transform","translateY(0px)");
+    $('li[class*="selected"]').toggleClass("selected");
+    $(`#${page}li`).toggleClass("selected");
   }
+};
+
+
+$('.music').click( function() {
+  return moveTabs(`${this.className}`);
 });
 
-$('.videos').click( function() {
-var pos = $(".videoContent").css("transform");
-if(pos != "matrix(1, 0, 0, 1, 0, 0)")
-{
-  $(".musicContent").css("transform","translateY(2000px)");
-  $(".picContent").css("transform","translateY(2000px)");
-  $(".bioContent").css("transform","translateY(2000px)");
-  $(".videoContent").css("transform","translateY(0px)");
-  $(navSelected).toggleClass("selected");
-  $('#videosli').toggleClass("selected");
-  navSelected = "#videosli";
-}
+$('.video').click( function() {
+  return moveTabs(`${this.className}`);
 });
 
-$('.pics').click( function() {
-var pos = $(".picContent").css("transform");
-if(pos != "matrix(1, 0, 0, 1, 0, 0)")
-{
-  $(".musicContent").css("transform","translateY(2000px)");
-  $(".videoContent").css("transform","translateY(2000px)");
-  $(".bioContent").css("transform","translateY(2000px)");
-  $(".picContent").css("transform","translateY(0px)");
-  $(navSelected).toggleClass("selected");
-  $('#picsli').toggleClass("selected");
-  navSelected = "#picsli";
-}
+$('.pic').click( function() {
+  return moveTabs(`${this.className}`);
 });
 
 $('.bio').click( function() {
-var pos = $(".bioContent").css("transform");
-if(pos != "matrix(1, 0, 0, 1, 0, 0)")
-{
-  $(".musicContent").css("transform","translateY(2000px)");
-  $(".videoContent").css("transform","translateY(2000px)");
-  $(".picContent").css("transform","translateY(2000px)");
-  $(".bioContent").css("transform","translateY(0px)");
-  $(navSelected).toggleClass("selected");
-  $('#bioli').toggleClass("selected");
-  navSelected = "#bioli";
-}
+  return moveTabs(`${this.className}`);
 });
-/*
-$('.merch').click( function() {
-var pos = $(".merchContent").css("transform");
-if(pos == "matrix(1, 0, 0, 1, 0, 0)")
-{
-  //$(".merchContent").css("transform","translateY(2000px)");
-}
-else {
-  $(".musicContent").css("transform","translateY(2000px)");
-  $(".videoContent").css("transform","translateY(2000px)");
-  $(".picContent").css("transform","translateY(2000px)");
-  $(".bioContent").css("transform","translateY(2000px)");
-  $(".merchContent").css("transform","translateY(0px)");
-}
-});
-*/
